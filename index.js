305db0b32f2f6491dee5de29e8d0b79cb0e2aa71
krishna5137 <inbox.krishna95@gmail.com>
2020-10-31 12:06:18 +0530

    feat: add create products

--- index.js
+++ index.js
@@ -17,15 +17,33 @@ mongoose.connect('mongodb://localhost:27017/farmInventory', { useNewUrlParser: t
         console.log(err)
     })
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
     res.render('products/index', { products });
 })
 
+app.get('/products/add', (req, res) => {
+    res.render('products/add')
+})
+
+app.post('/products', async (req, res) => {
+    const product = await new Product(req.body);
+    product.save();
+    res.redirect('/products')
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/details', { product });
+})
+
 app.listen(8080, () => {
     console.log("APP LISTENING ON 8080!!!")
-})
+});
