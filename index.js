211d2ba716c4c559d9a63aa35910bf30b81efd57
krishna5137 <inbox.krishna95@gmail.com>
2020-10-31 13:20:43 +0530

    feat: update inventory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 
 const Product = require('./models/product');
@@ -17,11 +18,14 @@ mongoose.connect('mongodb://localhost:27017/farmInventory', { useNewUrlParser: t
         console.log(err)
     })
 
+mongoose.set('useFindAndModify', false); // Make Mongoose use `findOneAndUpdate()`
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(methodOverride('_method')) // override with POST having ?_method=PUT
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
@@ -32,10 +36,24 @@ app.get('/products/add', (req, res) => {
     res.render('products/add')
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    console.log(product)
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const updateProduct = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    console.log(updateProduct);
+    res.redirect(`/products/${updateProduct._id}`);
+})
+
 app.post('/products', async (req, res) => {
-    const product = await new Product(req.body);
-    product.save();
-    res.redirect('/products')
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect(`/products/${product._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
